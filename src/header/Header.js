26c31a5333cec4e46998b6c14bc180aa4d7a2550
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-05-01 17:48:15 -0600

    arregle el commit

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import NavLinks from './NavLinks';
 import { NavLink } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () =>{
     return(
         <div className='header'>
                 <div className='header__desktopVersion'>
-                    <NavLink to='/' style={{
-                        color:'black',
-                        textDecoration:'none'
-                    }}>Erick M.M.A</NavLink>
+                    <NavLink className="navlink" to='/'>Erick M.M.A</NavLink>
                     <NavLinks/>
                 </div>
                 <div className='header__mobileVersion'>
--- src/main/Main.js
+++ src/main/Main.js
@@ -3,51 +3,17 @@ import {
     Route,
     Routes
   } from 'react-router-dom';
-import Hero from '../hero/Hero';
-const Principal = () =>{
-    return(
-        <div>
-            <Hero/>
-            <div className="Main__Tecnologías">
-                            <p>Tecnologías section</p>
-                        </div>
-                        <div className="Main__Tecnologías">
-                            <p>Sobre mí section</p>
-                        </div>
-                        <div className="Main__Portafolio">
-                            <p>Porfolio Section</p>
-                        </div>
-        </div>
-    )
-}
-const Porfolio = () =>{
-    return(
-        <div className="Main__Portafolio">
-            <p>Porfolio Main section</p>
-        </div>
-    )
-}
-const Sobremi = () =>{
-    return(
-        <div className="Main__SobreMi">
-            <p>Porfolio "Sobre mí" main section</p>
-        </div>
-    )
-}
-const Conectemos = () =>{
-    return(
-    <div className="Main__Conectemos">
-        <p>Conectemos section</p>
-    </div>
-    )
-}
+import Principal from '../components/Principal'
+import Portfolio from '../components/Portfolio';
+import Sobremi from '../components/Sobremi';
+import Conectemos from '../components/Conectemos';
 
 const Main = () =>{
     return(
         <main className='main'>
             <Routes>
                 <Route path='/' exact element={<Principal/>}/>
-                    <Route path='/porfolio' exact element={<Porfolio/>}/>
+                    <Route path='/porfolio' exact element={<Portfolio/>}/>
                 <Route path='/sobremi' exact element={<Sobremi/>}/>
                 <Route path='/conectemos' exact element={<Conectemos/>}/>
             </Routes>
