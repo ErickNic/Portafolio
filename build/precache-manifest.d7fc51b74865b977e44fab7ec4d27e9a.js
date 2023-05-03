98315a34452b61566c65d58c8e48916ff5234adb
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-05-02 20:18:19 -0600

    build folder done

---
+++ build/precache-manifest.d7fc51b74865b977e44fab7ec4d27e9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a8b00758384c3342da5",
+    "url": "/portafolio/static/css/main.495cc7cc.chunk.css"
+  },
+  {
+    "revision": "7a8b00758384c3342da5",
+    "url": "/portafolio/static/js/main.7a8b0075.chunk.js"
+  },
+  {
+    "revision": "440b31058b0715ca26d4",
+    "url": "/portafolio/static/js/1.440b3105.chunk.js"
+  },
+  {
+    "revision": "f6e89dd49bc11a475909",
+    "url": "/portafolio/static/js/runtime~main.f6e89dd4.js"
+  },
+  {
+    "revision": "3de58492847d3b70b547a21ce57a899b",
+    "url": "/portafolio/index.html"
+  }
+];
