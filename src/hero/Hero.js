4ddf7cf90283cd7212e403bf45fef45217330064
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-05-02 20:06:15 -0600

    build done

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -6,7 +6,9 @@ const Hero = () =>{
             <h1>Me llamo Erick Martin Meza Alonso</h1>
             <p>Soy un desarrollador web junior de mente abierta, optimista, dinámico,
                 creativo y siempre dispuesto a ayudar a otros a crecer y a cooperar ofreciendo 
-                lo mejor de mí en cada momento
+                lo mejor de mí en cada momento.
+
+                Me gustan trabajar en entornos simples, pero efectivos y prefiero tomar como referencia el "dicho menos es más" en cada uno de mis proyectos.
             </p>
         </div>
     );
