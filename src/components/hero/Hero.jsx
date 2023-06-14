0277ce7f4b0d74fe4a6f1b3111a56eca65b882c5
Erick Martin Meza Alonso <erickmartin.mezaalonso200245@gmail.com>
2023-06-14 14:52:50 -0600

    hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () =>{
     return(
         <div className={clases.hero__Container}>
             <h1 className={clases.hero__name}>Me llamo <span className={clases.hero__nameSpan}>Erick Martin Meza Alonso</span></h1>
-            <p className={clases.hero__caption}>Soy un <span><b>desarrollador web junior</b></span> de <span><b>mente abierta, optimista, dinámico, creativo </b></span>y sibpre dispuesto a ayudar a otros a crecer y a cooperar ofreciendo lo mejor de mí en cada momento.<br/><br/>Me gustan trabajar en <span><b>entornos simples, pero efectivos</b></span> entornos simples, pero efectivos y prefiero tomar como referencia el dicho<span> <b>menos es más</b></span> en cada uno de mis proyectos.
+            <p className={clases.hero__caption}>Soy un <span><b>desarrollador web junior</b></span> de <span><b>mente abierta, optimista, dinámico, creativo </b></span>y siempre dispuesto a ayudar a otros a crecer y a cooperar ofreciendo lo mejor de mí en cada momento.<br/><br/>Me gustan trabajar en <span><b>entornos simples, pero efectivos</b></span> entornos simples, pero efectivos y prefiero tomar como referencia el dicho<span> <b>menos es más</b></span> en cada uno de mis proyectos.
             </p>
         </div>
     );
