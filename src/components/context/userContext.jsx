4059a4b07bc4a7dbb9ee2b1f0eaa6ea88de4cb0c
Erick <erickmartin.mezaalonso200245@gmail.com>
2023-09-20 18:20:51 -0600

    xd

---
+++ src/components/context/userContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const sliderContext = createContext({
+    isLoggedIn: false,
+    logIn: () =>{},
+    logOut: () =>{}
+});
+
+const contextProvider = () =>{
+    
+}
